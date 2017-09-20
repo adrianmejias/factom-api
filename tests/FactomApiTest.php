89a823b76350fceebc4a7f058b37e9e946c29c4f
Adrian <adrianmejias@gmail.com>
2017-09-20 18:14:08 +0000

    Apply fixes from StyleCI

--- tests/FactomApiTest.php
+++ tests/FactomApiTest.php
@@ -2,13 +2,11 @@
 
 namespace AdrianMejias\FactomApi\Test;
 
-use Mockery;
 use PHPUnit\Framework\TestCase;
-use AdrianMejias\FactomApi\FactomApi;
 
 class FactomApiTest extends TestCase
 {
-    /** @test */
+    /* @test */
     // public function it_can_get_heights()
     // {
     //     $this->factomApi->heights();
